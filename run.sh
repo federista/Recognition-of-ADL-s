168391ada272aa9b65daf4e0279f3330f7ec824e
federista <zaidrafi107@gmail.com>
2022-06-04 03:00:18 +0500

    Video Upload

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-file_list=( "/home/zaid/project/PrepareData.py" "/home/zaid/project/TrainModel.py" "/home/zaid/project/PrepareSimulationData.py" "/home/zaid/project/IMU_Prediction.py" "/home/zaid/project/PredictionVoting.py" )
+file_list=( "/home/zaid/project/Recognition-of-ADL-s/PrepareData.py" "/home/zaid/project/Recognition-of-ADL-s/TrainModel.py" "//home/zaid/project/Recognition-of-ADL-s/PrepareSimulationData.py" "/home/zaid/project/Recognition-of-ADL-s/IMU_Prediction.py" "/home/zaid/project/Recognition-of-ADL-s/PredictionVoting.py" )
 
 
 for py_file in "${file_list[@]}"
