4f04a5f1fe9f2319fc0b8d8b0ceec0690d8f1244
federista <63880770+federista@users.noreply.github.com>
2022-07-15 19:22:33 +0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-file_list=( "/home/zaid/project/Recognition-of-ADL-s/PrepareData.py" "/home/zaid/project/Recognition-of-ADL-s/TrainModel.py" "//home/zaid/project/Recognition-of-ADL-s/PrepareSimulationData.py" "/home/zaid/project/Recognition-of-ADL-s/IMU_Prediction.py" "/home/zaid/project/Recognition-of-ADL-s/PredictionVoting.py" )
+file_list=( "/home/zaid/project/Recognition-of-ADL-s/PrepareData.py" "/home/zaid/project/Recognition-of-ADL-s/TrainModel.py" "//home/zaid/project/Recognition-of-ADL-s/PrepareSimulationData.py" "/home/zaid/project/Recognition-of-ADL-s/Model_Prediction.py" )
 
 
 for py_file in "${file_list[@]}"
